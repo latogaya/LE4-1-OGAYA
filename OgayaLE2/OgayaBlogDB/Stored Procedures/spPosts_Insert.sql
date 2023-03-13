14a83ef0d3f7466641fa16e18e42370b6ca856f0
Lex Ogaya <lexogaya@gmail.com>
2023-03-13 12:07:13 +0800

    LE4-1-OGAYA

---
+++ OgayaLE2/OgayaBlogDB/Stored Procedures/spPosts_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+@userId int,
+@title nvarchar(150),
+@body text,
+@dateCreated datetime2
+AS
+begin
+	INSERT INTO dbo.Posts
+	(UserId, TItle, Body, DateCreated)
+	VALUES
+	(@userId, @title, @body, @dateCreated)
+end
---
+++ OgayaLE2/OgayaBlogDB/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(16) NOT NULL, 
+    [FIrstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(16) NOT NULL
+)
